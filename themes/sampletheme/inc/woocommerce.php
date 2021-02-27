c5ea37d09af31cfaa6f266eed228bb611e54ae6a
Handan Zhang <handanzhang14@gmail.com>
2021-02-26 21:06:49 -0500

    Product single visual hook updated

---
+++ themes/sampletheme/inc/woocommerce.php
@@ -0,0 +1,34 @@
+<?php
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 1 );
+
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 0 );
+
+
+function sampletheme_woocommerce_single_product_summary_subtitle() {
+	echo '<b style="color:red;">Look at the amazing price!!!</b>';
+}
+add_action( 'woocommerce_single_product_summary', 'sampletheme_woocommerce_single_product_summary_subtitle', 2 );
+
+function sampletheme_woocommerce_before_add_to_cart_quantity_test() {
+	echo '<p>TEST</p>';
+}
+add_action( 'woocommerce_before_add_to_cart_quantity', 'sampletheme_woocommerce_before_add_to_cart_quantity_test', 5 );
+
+
+/**
+ * Updatte cart  w/ Ajax
+ */
+
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+	ob_start();
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> – <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
