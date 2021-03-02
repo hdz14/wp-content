62bfae756f8522e23fbc9b620b19896d9dcc6992
Handan Zhang <handanzhang14@gmail.com>
2021-03-02 05:57:16 -0500

    Woocommerce product single styling

--- themes/sampletheme/inc/woocommerce.php
+++ themes/sampletheme/inc/woocommerce.php
@@ -7,14 +7,14 @@ add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_e
 
 
 function sampletheme_woocommerce_single_product_summary_subtitle() {
-	echo '<b style="color:red;">Look at the amazing price!!!</b>';
+	echo '<b style="color:pink;">Look at the amazing price!!!</b>';
 }
 add_action( 'woocommerce_single_product_summary', 'sampletheme_woocommerce_single_product_summary_subtitle', 2 );
 
-function sampletheme_woocommerce_before_add_to_cart_quantity_test() {
-	echo '<p>TEST</p>';
-}
-add_action( 'woocommerce_before_add_to_cart_quantity', 'sampletheme_woocommerce_before_add_to_cart_quantity_test', 5 );
+// function sampletheme_woocommerce_before_add_to_cart_quantity_test() {
+// 	echo '<p>TEST</p>';
+// }
+// add_action( 'woocommerce_before_add_to_cart_quantity', 'sampletheme_woocommerce_before_add_to_cart_quantity_test', 5 );
 
 
 /**
