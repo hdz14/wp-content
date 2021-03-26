690ba80565d821b68271fbbea8782efbf77eeb01
Handan Zhang <handanzhang14@gmail.com>
2021-03-26 07:08:22 -0400

    All Pages custom styling final check and updated

--- themes/sampletheme/search.php
+++ themes/sampletheme/search.php
@@ -15,7 +15,7 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
+				<h2 style="margin-top: 2rem;" class="page-title">
 					<?php
 					/* translators: %s: search query. */
 					printf( esc_html__( 'Search Results for: %s', 'sampletheme' ), '<span>' . get_search_query() . '</span>' );
